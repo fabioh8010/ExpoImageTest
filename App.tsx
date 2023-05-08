fa59501497e27e8a15c1efff0dc74631ae0bd6db
ArekChr <arkadiuszchr@gmail.com>
2023-05-08 13:56:31 +0200

    custom pan pinch view

--- App.tsx
+++ App.tsx
@@ -1,117 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import type {PropsWithChildren} from 'react';
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  useWindowDimensions,
-  View,
-} from 'react-native';
-
 import {Image as ExpoImage} from 'expo-image';
-import ImageZoom from 'react-native-image-pan-zoom';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, useWindowDimensions} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {PanPinchView} from './PanPinchView';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    flex: 1,
-    backgroundColor: 'yellow',
-  };
-
+export default function App() {
   const dimensions = useWindowDimensions();
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ImageZoom
-        cropWidth={dimensions.width}
-        cropHeight={dimensions.height}
-        minScale={0.1}
-        imageWidth={width}
-        imageHeight={height}>
-        <ExpoImage
-          style={styles.image}
-          source={require('./tall_image.jpeg')}
-          contentFit={'contain'}
-          onLoad={event => {
-            console.log(event.source);
-            setWidth(event.source.width);
-            setHeight(event.source.height);
-          }}
-        />
-      </ImageZoom>
-    </SafeAreaView>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1, backgroundColor: 'green'}}>
+        <PanPinchView minZoom={1} maxZoom={10}>
+          <ExpoImage
+            style={[
+              styles.image,
+              {width: dimensions.width, height: dimensions.height},
+            ]}
+            source={require('./tall_image.jpeg')}
+            contentFit={'contain'}
+          />
+        </PanPinchView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
   image: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'yellow',
+    flex: 1,
   },
 });
-
-export default App;

6aa2d02cf3bb0e1a6c08339596b4e0a954198e2d
Arkadiusz Chrabąszczewski <Arkadiuszchr@gmail.com>
2023-05-17 12:16:17 +0200

    refactor: image transform

--- App.tsx
+++ App.tsx
@@ -1,15 +1,12 @@
 // import {Image as ExpoImage} from 'expo-image';
 import React, {useState} from 'react';
 import {View, useWindowDimensions} from 'react-native';
+import FastImage from 'react-native-fast-image';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {PanPinchView} from './PanPinchView';
-import FastImage from 'react-native-fast-image';
 
 export default function App() {
   const dimensions = useWindowDimensions();
-  // const [width, setWidth] = useState(dimensions.width);
-  // const [height, setHeight] = useState(dimensions.height);
-  // tall_image.jpeg dimensions, hardcoded for now but this is what Expo was giving and it matches image's dimensions.
   const [width, setWidth] = useState(875);
   const [height, setHeight] = useState(14894);
 
@@ -17,24 +14,12 @@ export default function App() {
     <GestureHandlerRootView style={{flex: 1, backgroundColor: 'green'}}>
       <PanPinchView minZoom={0.05} initialZoom={0.05} maxZoom={10}>
         <View style={{flex: 1, alignItems: 'center'}}>
-          {/* <ExpoImage
-            style={[{width: width, height: height}]}
-            onLoad={({source}) => {
-              setWidth(source.width);
-              setHeight(source.height);
-              console.log(source);
-            }}
-            source={require('./tall_image.jpeg')}
-            contentFit={'contain'}
-          /> */}
           <FastImage
             style={[{width: width, height: height}]}
             onLoad={e => {
-              // setWidth(source.width);
-              // setHeight(source.height);
               console.log(e.nativeEvent);
             }}
-            source={require('./tall_image.jpeg')}
+            source={require('./image-1.jpeg')}
           />
         </View>
       </PanPinchView>
